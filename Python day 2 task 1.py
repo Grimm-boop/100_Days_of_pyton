909a103722846f01f82214a9d969363ea54922af
Grimm-boop <alexmalikov19@gmail.com>
2024-11-14 16:51:11 +0200

    My python scripts

---
+++ Python day 2 task 1.py
@@ -0,0 +1,18 @@
+# Subscripting
+print("Henlo"[0])
+
+#String
+print('123 + 345')
+
+# Integer = whole number
+print(int(123 + 345))
+# Large Integers
+print(342_546_463)
+
+# Float = Floating Point Number
+print(3.1235)
+
+# Boolian
+print(True)
+print(False)
+
